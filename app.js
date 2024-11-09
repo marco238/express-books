c15dc77f0d9475f8c79ce40ea23795d1645e42e8
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2024-11-09 10:59:31 +0100

    class code

--- app.js
+++ app.js
@@ -14,13 +14,38 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
+
+
+
 app.get('/', (req, res, next) => {
-  Book.find()
+  const { q: searchTerm } = req.query
+
+  Book.find({ title: { $regex: new RegExp(searchTerm, "i") } })
     .then(books => {
       res.render('home', { books })
     })
     .catch(err => console.error(err))
 
+})
+
+app.get('/books/:id', (req, res, next) => {
+  const { id } = req.params
+
+  Book.findById(id)
+    .then(book => {
+      res.render('bookDetail', { book })
+    })
+    .catch(err => console.error(err))
+})
+
+app.get('/books/category/:category', (req, res, next) => {
+  const { category } = req.params
+
+  Book.find({ genres: { $in: category } })
+    .then(books => {
+      res.render('home', { books, category })
+    })
+    .catch(err => console.error(err))
 })
 
 
